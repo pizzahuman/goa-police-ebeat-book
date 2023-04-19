9587d97c19933c872673546f0ec8d7dfdd6608f2
sarthak <sarthaknaik010@gmail.com>
2023-04-19 13:36:51 +0530

    1st commit

---
+++ app.js
@@ -0,0 +1,38 @@
+require("./config/db/mongo");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const port = process.env.PORT || 3000;
+const app = express();
+
+
+
+// , credentials: true
+app.use(bodyParser.urlencoded({ extended: true }));
+// { origin: ['http://localhost:3000', 'https://docmeets.com', 'https://docmeets.netlify.app','https://docmeets-nextjs-app.netlify.app/' ,'https://docmeets-dev.netlify.app', 'https://docmeets-doctor-dashboard.netlify.app','https://docmeets.onrender.com'] }
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+
+//require routes
+const apiRoutes = require('./routes/index');
+app.use('/', apiRoutes);
+
+setInterval(function () {
+    console.log("pinged");
+}, 600000)
+
+app.get("/", (req, res) => {
+    res.send(`app working ` + new Date().toLocaleTimeString());
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
+
+module.exports = app;
+
+
---
+++ config/db/mongo.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_URI;
+
+const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+// Connect MongoDB Atlas using mongoose connect method
+const conn = mongoose.connection;
+
+conn.openUri(uri, options).then(() => {
+    console.log("Database connection established!!");
+},
+    err => {
+        {
+            console.log("Error connecting Database instance due to:", err);
+        }
+    })
+
+module.exports = conn;
