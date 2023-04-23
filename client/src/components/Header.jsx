a267e8509a2ffa2346b73a403db5310f2687b242
Prateek Sharma <sharmaprateek.dev@gmail.com>
2023-04-23 16:43:53 +0530

    1st commit

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import gog from "../images/gog.png";
+import gp from "../images/gp.png";
+
+function Header(){
+    return(
+        <>
+                <div className="header">
+        <header className="flex-row-center-all">
+            <div className="img-holder flex-row-center-all">
+                <div className="header-img">
+                    <img src={gog} alt="" />
+                </div>
+                <div className="header-img">
+                    <img src={gp} alt="" />
+                </div>
+            </div>
+        </header>
+    </div>
+        </>
+    );
+}
+
+export default Header;
